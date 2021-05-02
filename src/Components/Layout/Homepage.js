95ef8a04998168131cf49b0f0609965169237bc5
Smack <kovacsmyles1@gmail.com>
2021-05-02 18:16:29 -0500

    updated homepage

--- src/Components/Layout/Homepage.js
+++ src/Components/Layout/Homepage.js
@@ -9,7 +9,7 @@ function Homepage() {
                 <img src={selfie} class="card-img-top" alt="..."/>
                     <div class="card-body">
                         <h5 class="card-title">Myles Kovacs</h5>
-                        <p class="card-text">A full stack web developer</p>
+                        <p class="card-text">A full stack web developer looking to make dreams come true through websites! Trained for the MERN stack but able to branch into any language given time!</p>
                         <a href="/Projects" class="btn btn-primary">Projects</a>
                     </div>
         </div>
